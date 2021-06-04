626ae6d10f3dd6b66bdd09b25167c917399e84b9
youngho-j <alfkwl1239@naver.com>
2021-06-04 15:57:42 +0900

    문자열 계산기 만들기
    1. 요구사항3 : 숫자 두개를 구분자(쉼표)로 입력시 숫자의 합을 반환
    2. 요구사항3 테스트 코드 구현

--- yh_2nd_onion/src/calculator/StringCalculator.java
+++ yh_2nd_onion/src/calculator/StringCalculator.java
@@ -6,6 +6,15 @@ public class StringCalculator {
 		if(text == null || text.isEmpty()) {
 			return 0;			
 		}
+		// 요구사항 3. 숫자 두개를 구분자(쉼표)로 입력시 숫자의 합을 반환
+		if(text.contains(",")) {
+			String[] values = text.split(",");
+			int sum = 0;
+			for(String val : values) {
+				sum += Integer.parseInt(val);
+			}
+			return sum;
+		}
 		// 요구사항 2. 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환
 		return Integer.parseInt(text);
 	}
--- yh_2nd_onion/test/calculator/StringCalculatorTest.java
+++ yh_2nd_onion/test/calculator/StringCalculatorTest.java
@@ -23,5 +23,10 @@ public class StringCalculatorTest {
 	public void 숫자하나_문자열_입력시_해당_숫자반환_테스트() {
 		assertEquals(10, cal.add("10"));
 	}
+	
+	@Test
+	public void 구분자_쉼표로_입력시_숫자의합_반환_테스트() {
+		assertEquals(9, cal.add("3,2,4"));
+	}
 
 }
