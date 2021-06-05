5f183383c55ea9114a601214489c8f98c866b2c2
youngho-j <alfkwl1239@naver.com>
2021-06-05 16:12:36 +0900

    문자열 계산기 만들기
    1. 요구사항3 리팩토링 : sum 메서드 역할 분리(변환, 숫자합)

--- yh_2nd_onion/src/calculator/StringCalculator.java
+++ yh_2nd_onion/src/calculator/StringCalculator.java
@@ -1,8 +1,9 @@
 package calculator;
 
 public class StringCalculator {
-
-	int add(String text) {
+	// 리팩토링 시 주의 깊게 살펴볼 부분은 private 메서드가 아니라 
+	// public으로 공개하고 있는 메서드가 얼마나 읽기 쉽고 좋은가가 가장 중요!
+	public int add(String text) {
 		
 		// 요구사항 1. 빈 문자열 or null 값 입력시 0 반환
 		if(text == null || text.isEmpty()) {
@@ -14,12 +15,26 @@ public class StringCalculator {
 
 		String[] values = text.split(",");
 		
+		return sum(toInts(values));
+		
+	}
+	// 요구사항3 리팩토링 (add 메소드 내 역할 분리) - sum 메소드 역할 분리 필요(변환 + 합치기)  
+	private int sum(int[] values) {
 		int sum = 0;
-		for(String val : values) {
-			sum += Integer.parseInt(val);
+		for(int val : values) {
+			sum += val;
+		}
+		return sum;
+	}
+	
+	// 요구사항3 리팩토링 - 문자열을 숫자로 변환
+	private int[] toInts(String[] values) {
+		int[] arr = new int[values.length];
+		
+		for(int i = 0 ; i < values.length ; i++) {
+			arr[i] = Integer.parseInt(values[i]);
 		}
 		
-		return sum;
-		
+		return arr;
 	}
 }
