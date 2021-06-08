534ba65a6622ac8fd2d1ce04ddbf66eb5d9e7f52
youngho-j <alfkwl1239@naver.com>
2021-06-08 16:50:08 +0900

    사다리 타기 구현
    1. 기존 1차원 배열 -> 2차원 배열 구조로 변경
    2. 테스트 코드 작성(단위 테스트 통과)

--- ladder/src/ladder/Ladder.java
+++ ladder/src/ladder/Ladder.java
@@ -9,33 +9,42 @@ public class Ladder {
 	 *    1 => 0
 	 *  * 우로 이동할 수 있는가?
 	 *    0 => 1
-	 *    
+	 *  
+	 *  여러개의 행을 추가할 수 있도록 자원
+	 *  -> 2차원 배열로 변경
+	 *  
 	*/
-	private int[] row;
+	private int[][] rows;
 	
 	// 사다리타기 참여 인원만큼 배열 길이 지정
-	public Ladder(int numOfPerson) {
-		row = new int[numOfPerson];
+	public Ladder(int countOfRow, int numOfPerson) {
+		rows = new int[countOfRow][numOfPerson];
 	}
+	
 	// 이동 선 긋기
-	public void drawLine(int startPosition) {
-		row[startPosition] = 1;
-		row[startPosition + 1] = 1;
+	public void drawLine(int numOfRow,int startPosition) {
+		rows[numOfRow][startPosition] = 1;
+		rows[numOfRow][startPosition + 1] = 1;
 	}
 	
 	public int run(int numOfPerson) {
-		if(row[numOfPerson] == 0) {
-			return numOfPerson;
+		for(int i = 0 ; i < rows.length ; i++) {
+			int[] row = rows[i];
+			
+			if(row[numOfPerson] == 0) {
+				continue;
+			}
+			
+			if(numOfPerson - 1 >= 0) {
+				int leftValue = row[numOfPerson - 1];
+				if(leftValue == 1) {
+					numOfPerson -= 1;
+					continue;
+				}			
+			}
+			// 좌측 이동이 아니면 우측이므로.. 
+			numOfPerson += 1;
 		}
-		
-		if(numOfPerson - 1 >= 0) {
-			int leftValue = row[numOfPerson - 1];
-			if(leftValue == 1) {
-				return numOfPerson - 1;
-			}			
-		}
-		// 좌측 이동이 아니면 우측이므로.. 
-		return numOfPerson + 1;
-		
+		return numOfPerson;
 	}
 }
--- ladder/test/ladder/LadderTest.java
+++ ladder/test/ladder/LadderTest.java
@@ -8,7 +8,7 @@ public class LadderTest {
 	
 	@Test
 	public void testRunWhenNoLine()  throws Exception {
-		Ladder ladder = new Ladder(3);
+		Ladder ladder = new Ladder(1, 3);
 		int target = ladder.run(0);
 		assertEquals(0, target);
 	}
@@ -16,14 +16,14 @@ public class LadderTest {
 	@Test
 	public void testRunWhenLineLeft() throws Exception {
 		// 1 1 0
-		Ladder ladder = new Ladder(3);
-		ladder.drawLine(1);
+		Ladder ladder = new Ladder(1, 3);
+		ladder.drawLine(0, 1);
 		
 		int target = ladder.run(2);
 		assertEquals(1, target);
 		// 0 1 1
-		ladder = new Ladder(3);
-		ladder.drawLine(1);
+		ladder = new Ladder(1, 3);
+		ladder.drawLine(0, 1);
 		
 		target = ladder.run(2);
 		assertEquals(1, target);
@@ -32,16 +32,33 @@ public class LadderTest {
 	@Test
 	public void testRunWhenLineRight() throws Exception {
 		// 0 1 1
-		Ladder ladder = new Ladder(3);
-		ladder.drawLine(1);
+		Ladder ladder = new Ladder(1, 3);
+		ladder.drawLine(0, 1);
 		
 		int target = ladder.run(1);
 		assertEquals(2, target);
 		// 1 1 0
-		ladder = new Ladder(3);
-		ladder.drawLine(0);
+		ladder = new Ladder(1, 3);
+		ladder.drawLine(0, 0);
 		
 		target = ladder.run(0);
 		assertEquals(1, target);
+	}
+	@Test
+	public void testRunWhenMultiRows() throws Exception {
+		// 1 1 0 0
+		// 0 1 1 0
+		// 0 0 1 1
+		Ladder ladder = new Ladder(3, 4);
+		ladder.drawLine(0, 0);
+		ladder.drawLine(1, 1);
+		ladder.drawLine(2, 2);
+		
+		assertEquals(3, ladder.run(0));
+		assertEquals(0, ladder.run(1));
+		assertEquals(1, ladder.run(2));
+		assertEquals(2, ladder.run(3));
+		
+		
 	}
 }
