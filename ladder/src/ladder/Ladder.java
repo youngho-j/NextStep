ea5aec10833a6208c45b88bdf831657a645e5949
youngho-j <alfkwl1239@naver.com>
2021-06-08 16:33:24 +0900

    사다리 타기 구현
    1. 기존 작성 코드 제거(파일명 변경- LadderFirst, LadderFirstTest)
    2. 요구사항 변경하여 구현
      - 좌우 이동 불가능한 경우
      - 좌로 이동 가능한 경우
      - 우로 이동 가능한 경우

--- ladder/src/ladder/Ladder.java
+++ ladder/src/ladder/Ladder.java
@@ -1,31 +1,41 @@
 package ladder;
 
 public class Ladder {
+
+	/*
+	 *  0 - 좌/우 이동 X, 0 => 0
+	 *  1
+	 *  * 좌로 이동할 수 있는가?
+	 *    1 => 0
+	 *  * 우로 이동할 수 있는가?
+	 *    0 => 1
+	 *    
+	*/
 	private int[] row;
 	
-	Ladder(int numOfPerson) {
+	// 사다리타기 참여 인원만큼 배열 길이 지정
+	public Ladder(int numOfPerson) {
 		row = new int[numOfPerson];
 	}
-	
-	public void drawLine(int StartPosition) {
-		row[StartPosition] = 1;
-		row[StartPosition + 1] = 1;
+	// 이동 선 긋기
+	public void drawLine(int startPosition) {
+		row[startPosition] = 1;
+		row[startPosition + 1] = 1;
 	}
-
+	
 	public int run(int numOfPerson) {
-		if(numOfPerson - 1 <= 0 && row[numOfPerson - 1] == 1) {
-			return numOfPerson + 1;
+		if(row[numOfPerson] == 0) {
+			return numOfPerson;
 		}
 		
-		if(row[numOfPerson - 1] == 1) {
-			if(row[numOfPerson - 2] == 1) {
-				return numOfPerson -1;
-			}
-			if(row[numOfPerson] == 1) {
-				return numOfPerson + 1;
-			}
+		if(numOfPerson - 1 >= 0) {
+			int leftValue = row[numOfPerson - 1];
+			if(leftValue == 1) {
+				return numOfPerson - 1;
+			}			
 		}
-		return numOfPerson;
+		// 좌측 이동이 아니면 우측이므로.. 
+		return numOfPerson + 1;
+		
 	}
-
 }
--- ladder/test/ladder/LadderTest.java
+++ ladder/test/ladder/LadderTest.java
@@ -1,65 +1,47 @@
 package ladder;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.*;
 
-public class LadderTest extends TestCase {
-	// 요구사항 : 1 by 1, 1 by 0, 1 by 2 / 1 => 1
-	// 해당 테스트 진행시 NoClassDefFoundError 발생하는 경우
-	// 원인 : JUnit 추가시 Modulepath에 추가해서
-	// 해결방법 : 기존 추가한 JUnit 제거 후 ClassPath에 추가하면 됨
-	public void testRunWhenOneByOne() throws Exception {
-		Ladder ladder = new Ladder(1);
-				
-		int target = ladder.run(1);
-		
-		assertEquals(1, target);
-	}
-	
-	// 요구사항 : 2 by 1 (사용자 2명에 선이 하나)
-	// 선이 그어져 있지 않은 경우 : 0 0 / 1 => 1, 2 => 2
-	public void testRunWhenTwoByOneNoLine() throws Exception {
-		Ladder ladder = new Ladder(2);
-		
-		int target = ladder.run(1);
-		assertEquals(1, target);
+import org.junit.Test;
 
-		target = ladder.run(2);
-		assertEquals(2, target);
-	}
+public class LadderTest {
 	
-	public void testRunWhenTwoByOneOneLine() throws Exception {
-		Ladder ladder = new Ladder(2);
-		ladder.drawLine(0);
-		int target = ladder.run(1);
-		assertEquals(2, target);
-		
-		target = ladder.run(2);
-		assertEquals(1, target);
-	}
-	
-	// 요구사항 : 3 by 1 
-	// 테스트 실패시 로직을 추가해야 되는구나라고 알 수 있음
-	public void testRunWhenThreeByOneOneLine() throws Exception {
-		// 1 1 0 의 경우
+	@Test
+	public void testRunWhenNoLine()  throws Exception {
 		Ladder ladder = new Ladder(3);
-		ladder.drawLine(0);
-		int target = ladder.run(2);
-		assertEquals(1, target);
-		target = ladder.run(1);
-		assertEquals(2, target);
-		target = ladder.run(3);
-		assertEquals(3, target);
+		int target = ladder.run(0);
+		assertEquals(0, target);
 	}
 	
-	public void testRunWhenThreeByOneOneLine2() throws Exception {
-		// 0 1 1 의 경우
+	@Test
+	public void testRunWhenLineLeft() throws Exception {
+		// 1 1 0
 		Ladder ladder = new Ladder(3);
 		ladder.drawLine(1);
-		int target = ladder.run(1);
+		
+		int target = ladder.run(2);
 		assertEquals(1, target);
+		// 0 1 1
+		ladder = new Ladder(3);
+		ladder.drawLine(1);
+		
 		target = ladder.run(2);
-		assertEquals(3, target);
-		target = ladder.run(3);
+		assertEquals(1, target);
+		
+	}
+	@Test
+	public void testRunWhenLineRight() throws Exception {
+		// 0 1 1
+		Ladder ladder = new Ladder(3);
+		ladder.drawLine(1);
+		
+		int target = ladder.run(1);
 		assertEquals(2, target);
+		// 1 1 0
+		ladder = new Ladder(3);
+		ladder.drawLine(0);
+		
+		target = ladder.run(0);
+		assertEquals(1, target);
 	}
 }
