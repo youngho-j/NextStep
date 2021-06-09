2dd39f2d2d48af4114b05e327f78defa1eca97f4
youngho-j <alfkwl1239@naver.com>
2021-06-09 16:00:18 +0900

    사다리 타기 만들기
    리팩토링
    
    1. isRight메서드 이름 Rename
    2. 오른쪽, 왼쪽 이동 값 (1, -1) 상세화 - 겹치는 값이 존재하므로
    2-1. 상수처리한 값을 enum으로 변경

--- ladder/src/ladder/Row.java
+++ ladder/src/ladder/Row.java
@@ -1,6 +1,34 @@
 package ladder;
 
+/*
+ * 리팩토링
+ * 메서드 이름 리팩토링
+ * 1, -1 값을 명확히 한다.(상수 처리)
+ * 상수 값을 enum으로 변경(상수 값이 여러개일 경우)
+ */
+
 public class Row {
+	// enum - 클래스 안, 밖에 만들 수 있음.. 여기에선 안에서만 사용하므로 내부에 만들예정
+	enum Direction {
+		LEFT(-1),
+		CENTER(0),
+		RIGHT(1);
+		
+		private int num;
+		
+		private Direction(int num) {
+			this.num = num;
+		}
+		
+		public int getNum() {
+			return num;
+		}
+	}
+	
+//	private static final int LEFT_DIRECTION = -1;
+//	private static final int RIGHT_DIRECTION = 1;
+//	private static final int CENTER_DIRECTION = 0;
+	
 	int[] persons;
 	
 	Row(int numOfPerson){
@@ -9,9 +37,9 @@ public class Row {
 	
 	void drawLine(int startPosition) {
 		// 오른쪽으로 이동하는 선
-		persons[startPosition] = 1;
+		persons[startPosition] = Direction.RIGHT.getNum();
 		// 왼쪽으로 이동하는 선
-		persons[startPosition + 1] = -1;
+		persons[startPosition + 1] = Direction.LEFT.getNum();
 	}
 	
 	public int move(int numOfPerson) {
@@ -19,18 +47,18 @@ public class Row {
 			return numOfPerson;
 		}
 		// 숫자가 1이면 오른쪽으로 이동 가능
-		if(isRight(numOfPerson)) {
+		if(isRightDirection(numOfPerson)) {
 			return numOfPerson + 1;
 		}
 		// 그 외의 경우(0, 1이 아닌경우)
 		return numOfPerson - 1;		
 	}
 
-	private boolean isRight(int numOfPerson) {
-		return persons[numOfPerson] == 1;
+	private boolean isRightDirection(int numOfPerson) {
+		return persons[numOfPerson] == Direction.RIGHT.getNum();
 	}
 
 	private boolean isNoLine(int numOfPerson) {
-		return persons[numOfPerson] == 0;
+		return persons[numOfPerson] == Direction.CENTER.getNum();
 	}
 }
